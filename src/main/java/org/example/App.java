09c820a95dbd45ae35145b67abcc517cd6166090
robinalfengard <robin.alfengard@hotmail.com>
2023-09-07 14:38:58 +0200

    added Locale.setDefault to US in app.java

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        // test
+        // testa
         Locale.setDefault(Locale.US);
         Menu.run();
 
