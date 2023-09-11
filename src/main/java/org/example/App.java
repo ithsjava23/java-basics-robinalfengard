7f37b9d8b468f2af9e265d3792633841f7f7d76a
robinalfengard <robin.alfengard@hotmail.com>
2023-09-11 14:57:34 +0200

    visualization modifications

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,6 +7,12 @@ public class App {
 
     public static void main(String[] args) {
         // test again
+        //TODO
+        // Visualisering
+        // felhantering med try/catch
+        // formatera 00orna
+        // Städa & organisera
+
         Locale swedishLocale = new Locale("sv", "SE");
         Locale.setDefault(swedishLocale);
         Menu.run();
--- src/main/java/org/example/VisualizeMethods.java
+++ src/main/java/org/example/VisualizeMethods.java
@@ -10,16 +10,16 @@ public class VisualizeMethods {
         ArrayList<ListOfHoursAndPrices> sortedListOfHoursAndPrices = new ArrayList<>(hourAndPriceCombined);
         sortedListOfHoursAndPrices.sort(Comparator.comparingInt(ListOfHoursAndPrices::getPrice).reversed());
 
-
+        // Get highest and lowest value
         int highestValue = sortedListOfHoursAndPrices.get(0).price;
         int lowestValue =  sortedListOfHoursAndPrices.get(sortedListOfHoursAndPrices.size()-1).price;
 
 
-    //    int everyFifth = (highestValue - lowestValue)/NUMBER_OF_ROWS;
-        double everyFifth = (lowestValue > 0) ? (double) (highestValue - lowestValue) / NUMBER_OF_ROWS : (double) (highestValue + lowestValue) / NUMBER_OF_ROWS;
-
-
-
+        // Calculate distance between separators
+        double everyFifth = (lowestValue > 0) ? (double) (highestValue - lowestValue) / NUMBER_OF_ROWS : (double) (highestValue + Math.abs(lowestValue)) / NUMBER_OF_ROWS;
+        double newNumber = (highestValue-everyFifth);
+        int newNumberRounded = (int)newNumber;
+        // Print highest value and first loop with check for data that contains the highest value
         System.out.print("   " + highestValue + "|");
         for (ListOfHoursAndPrices listOfHoursAndPrices : hourAndPriceCombined) {
             if (listOfHoursAndPrices.getPrice() == highestValue){
@@ -27,43 +27,28 @@ public class VisualizeMethods {
             } else System.out.print("   ");
         }
         System.out.print("\n");
-        double newNumber = highestValue-everyFifth;
-        System.out.print("   |");
-        for (ListOfHoursAndPrices listOfHoursAndPrices : hourAndPriceCombined) {
-            if (listOfHoursAndPrices.getPrice()>=newNumber && listOfHoursAndPrices.getPrice()<highestValue || listOfHoursAndPrices.getPrice() ==highestValue){
-                System.out.print("  x");
-            }else System.out.print("   ");
+
+        // Print and check for the data between the lowest and high values
+
+        for(int i = 0; i<4; i++){
+                System.out.print("   |");
+            for (ListOfHoursAndPrices listOfHoursAndPrices : hourAndPriceCombined) {
+                if (listOfHoursAndPrices.getPrice()>=newNumberRounded && listOfHoursAndPrices.getPrice()<highestValue || listOfHoursAndPrices.getPrice() ==highestValue){
+                    System.out.print("  x");
+                }else System.out.print("   ");
+            }
+            System.out.print("\n");
+            newNumber = (newNumberRounded - everyFifth);
+
+            //Adjustment since I subtract instead of add
+            if(newNumber<33){
+                newNumberRounded = (int) Math.ceil(newNumber);
+            } else newNumberRounded = (int) newNumber;
+
         }
-        System.out.print("\n");
 
 
-        newNumber = newNumber-everyFifth;
-        System.out.print("   |");
-        for (ListOfHoursAndPrices listOfHoursAndPrices : hourAndPriceCombined) {
-            if (listOfHoursAndPrices.getPrice()>=newNumber && listOfHoursAndPrices.getPrice()<highestValue || listOfHoursAndPrices.getPrice() ==highestValue){
-                System.out.print("  x");
-            }else System.out.print("   ");
-        }
-        System.out.print("\n");
-
-
-        newNumber = newNumber-everyFifth;
-        System.out.print("   |");
-        for (ListOfHoursAndPrices listOfHoursAndPrices : hourAndPriceCombined) {
-            if (listOfHoursAndPrices.getPrice()>=newNumber && listOfHoursAndPrices.getPrice()<highestValue || listOfHoursAndPrices.getPrice() ==highestValue){
-                System.out.print("  x");
-            }else System.out.print("   ");
-        }
-        System.out.print("\n");
-        newNumber = newNumber-everyFifth;
-        System.out.print("   |");
-        for (ListOfHoursAndPrices listOfHoursAndPrices : hourAndPriceCombined) {
-            if (listOfHoursAndPrices.getPrice()>=newNumber && listOfHoursAndPrices.getPrice()<highestValue || listOfHoursAndPrices.getPrice() ==highestValue){
-                System.out.print("  x");
-            }else System.out.print("   ");
-        }
-        System.out.print("\n");
-
+        // Print lowest value and check for data that is equal to the lowest value
         System.out.print((lowestValue < 0) ? lowestValue + "|" : " " + lowestValue + "|");
         for (ListOfHoursAndPrices listOfHoursAndPrices : hourAndPriceCombined) {
             if (listOfHoursAndPrices.getPrice() == lowestValue || listOfHoursAndPrices.getPrice()>=lowestValue){
@@ -72,16 +57,12 @@ public class VisualizeMethods {
         }
         System.out.print("\n");
 
-
+        // Print a long line and print the hours of the day
         System.out.print("   |------------------------------------------------------------------------\n");
         System.out.print("   | ");
         for(int hour = 0; hour<hourAndPriceCombined.size(); hour++){
             System.out.printf("%02d ", hour);
         }
         System.out.print("\n");
-
     }
-
-
-
 }
